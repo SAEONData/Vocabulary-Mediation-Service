fa2d4fec6e22cdc5e4092d4260b97699046080cb
andree1985 <30987911+andree1985@users.noreply.github.com>
2018-08-01 15:16:55 +0200

    Add project files.

---
+++ VocabularyMediationService/Controllers/LookupController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using VocabularyMediationService.Providers;
+
+namespace VocabularyMediationService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api")]
+    public class LookupController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public LookupController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
+        [Route("services")]
+        [HttpGet]
+        public List<string> Services()
+        {
+            var supportedVocabServices = new List<string>()
+            {
+                "orcid",
+                "re3data",
+                "sagdad",
+                "geonames",
+                "obo",
+                "worms"
+            };
+
+            return supportedVocabServices.OrderBy(x => x).ToList();
+        }
+
+        [Route("search/{providerName}/{searchPhrase}")]
+        [HttpGet]
+        public string Search(string providerName, string searchPhrase)
+        {
+            string result = "success";
+
+            switch (providerName.ToLower())
+            {
+                case "orcid":
+                    var provider = new ProviderOrcid("", "");
+                    break;
+            }
+
+            return result; //$"Provider: {provider}, Phrase: {phrase}";
+        }
+    }
+}
---
+++ VocabularyMediationService/Providers/ProviderOrcid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VocabularyMediationService.Providers
+{
+    public class ProviderOrcid
+    {
+        private string _clientId { get; }
+        private string _clientSecret { get; }
+
+        public ProviderOrcid(string clientId, string clientSecret)
+        {
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+        }
+
+
+    }
+}
